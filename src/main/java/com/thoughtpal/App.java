d52e3f42529e2c16ede4ba1627a14386fe010734
robwood0412 <rob.a.wood@gmail.com>
2016-04-30 08:25:58 -0600

    Refactor App as @SpringBootApplication

--- src/main/java/com/thoughtpal/App.java
+++ src/main/java/com/thoughtpal/App.java
@@ -1,13 +1,15 @@
 package com.thoughtpal;
 
-/**
- * Hello world!
- *
- */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(new Object[] { App.class }, args);
     }
 }
