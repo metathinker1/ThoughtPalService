0bcd21b21f604c63a44a94cb3d880872db3a0177
Robert Wood <rob.a.wood@gmail.com>
2017-09-07 21:58:58 -0600

    Render as HTML rather than text

--- src/main/java/com/thoughtpal/func/NoteDocSummaryRenderer.java
+++ src/main/java/com/thoughtpal/func/NoteDocSummaryRenderer.java
@@ -8,23 +8,26 @@ public class NoteDocSummaryRenderer {
 
     public String render(NoteDocument noteDoc)
     {
-         /*
-         *  Design:
-         *    Return HTML String
-         *    <h2>Note.summaryText()</h2>; <h3>Tag.summaryText()</h3>
-         */
-         StringBuilder outline = new StringBuilder();
-         noteDoc.getNoteDocItems().stream()
-                //.forEach(System.out::println);
-                .forEach(noteDocItem -> {
-                    if (noteDocItem instanceof Note) {
-                        Note note = (Note) noteDocItem;
-                        outline.append(note.getLabel() + ": " + note.getSummaryText() + "\n\n");
-                    } else {
-                        Tag tag = (Tag) noteDocItem;
-                        outline.append(tag.getSummaryText() + "\n\n");
-                    }
-                });
+        /*
+        *  Design:
+        *    Return HTML String
+        *    <h2>Note.summaryText()</h2>; <h3>Tag.summaryText()</h3>
+        */
+        StringBuilder outline = new StringBuilder();
+        outline.append("<!DOCTYPE html><html lang=\"en\"><head><title>Example 01 - Outline</title><link rel=\"stylesheet\" type=\"text/css\" href=\"./css/outlinestyle.css\"></head>");
+        outline.append("<body>");
+        noteDoc.getNoteDocItems().stream()
+            //.forEach(System.out::println);
+            .forEach(noteDocItem -> {
+                if (noteDocItem instanceof Note) {
+                    Note note = (Note) noteDocItem;
+                    outline.append("<h2>" + note.getLabel() + ": " + note.getSummaryText() + "</h2><br><br>");
+                } else {
+                    Tag tag = (Tag) noteDocItem;
+                    outline.append("<h3>" + tag.getSummaryText() + "</h3><br><br>");
+                }
+            });
+        outline.append("\n</body></html>");
         return outline.toString();
     }
 }
