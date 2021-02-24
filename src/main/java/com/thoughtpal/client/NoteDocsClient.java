786e7b4ff6d13be0f08d2ea32bea7376f9c443df
Rob Wood <metathinker1@gmail.com>
2021-02-24 07:36:00 -0700

    Get documents from local Google Drive directory rather than ThoughtPalUI

--- src/main/java/com/thoughtpal/client/NoteDocsClient.java
+++ src/main/java/com/thoughtpal/client/NoteDocsClient.java
@@ -9,7 +9,10 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Created by robertwood on 1/28/17.
@@ -19,7 +22,23 @@ public class NoteDocsClient
     // TODO: DI
     private final HttpClient    client = HttpClients.createDefault();
 
-    public NoteDocumentText getNoteDocument(String directory, String fileName) throws Exception
+    public NoteDocumentText getNoteDocument(String directory, String fileName) throws Exception {
+        String homeDirectory = "/Users/robertwood/Google Drive/NoteDocRepo/";
+        String filePath = homeDirectory + directory + '/' + fileName;
+        //new String(Files.readAllBytes(Paths.get("duke.java")));
+        try {
+            String rawText = new String(Files.readAllBytes(Paths.get(filePath)));
+            System.out.println(rawText);
+            String workspaceId = Integer.toString(directory.hashCode());
+            String noteDocumentId = Integer.toString(fileName.hashCode());
+            return new NoteDocumentText(workspaceId, noteDocumentId, rawText);
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            throw new Exception(excp.getMessage());
+        }
+    }
+
+    public NoteDocumentText getNoteDocument_FromServer(String directory, String fileName) throws Exception
     {
         // curl http://localhost:8080/AppDev/AppDevAPI.JavaServlet.nodoc
         //String uriPath = "http://localhost:5011/" + workspace + "/" + noteDocName;
